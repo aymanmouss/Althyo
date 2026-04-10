0be41cac5dd0bf630e71f84f8355587a19a0e268
Ayman <moussa.aymen@gmail.com>
2026-04-11 00:05:48 +0200

    chore: replace outdated migration with new schema definition

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20260410_193023 from './20260410_193023';
+import * as migration_20260410_220534 from './20260410_220534';
 
 export const migrations = [
   {
-    up: migration_20260410_193023.up,
-    down: migration_20260410_193023.down,
-    name: '20260410_193023'
+    up: migration_20260410_220534.up,
+    down: migration_20260410_220534.down,
+    name: '20260410_220534'
   },
 ];
